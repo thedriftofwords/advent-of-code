083cef54fa8c7f3bb22b9cb25ffe1309268bc61d
Crystal Gomes <crystal.gomes@gmail.com>
2020-12-04 14:54:05 +0000

    Refactored and cleaned up Day 4

--- 04.py
+++ 04.py
@@ -1,120 +1,106 @@
-class Passport:
+import functools
 
-  def __init__(self, birth_year, issue_year, expiration_year, height, hair_color, eye_color, passport_id, country_id=None):
-    self.birth_year = birth_year
-    self.issue_year = issue_year
-    self.expiration_year = expiration_year
-    self.height = height
-    self.hair_color = hair_color
-    self.eye_color = eye_color
-    self.passport_id = passport_id
-    self.country_id = country_id
+valid_years = {
+  "eyr": {"min": 2020, "max": 2030},
+  "iyr": {"min": 2010, "max": 2020},
+  "byr": {"min": 1920, "max": 2002}
+}
+valid_pid_len = 9
+valid_eye_colors = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
 
-  valid_years = {
-    "eyr": {"min": 2020, "max": 2030},
-    "iyr": {"min": 2010, "max": 2020},
-    "byr": {"min": 1920, "max": 2002}
-  }
-  valid_pid_len = 9
-  valid_eye_colors = {"amb", "blu", "brn",                        "gry", "grn", "hzl", "oth"}
+def is_eye_color_valid(eye_color):
+  return eye_color in valid_eye_colors
 
-  def is_eye_color_valid(eye_color):
-    return eye_color in Passport.valid_eye_colors
+def is_pid_valid(pid):
+  return len(pid) == valid_pid_len
 
-  def is_pid_valid(pid):
-    return len(pid) == Passport.valid_pid_len
-
-  def are_all_years_valid(passport):
-    "Checks validity of birth, issue and expiration years"
-    year_keys = Passport.valid_years.keys()
-    for key in year_keys:
-      passport[key] = int(passport[key])
-      if not (Passport.valid_years[key]["min"] <= passport[key] <= Passport.valid_years[key]["max"]):
-        return False
-    return True
-
-  def is_hair_color_valid(hair_color):
-    if len(hair_color) != 7:
+def are_all_years_valid(passport):
+  "Checks validity of birth, issue and expiration years"
+  year_keys = valid_years.keys()
+  for key in year_keys:
+    passport[key] = int(passport[key])
+    if not (valid_years[key]["min"] <= passport[key] <= valid_years[key]["max"]):
       return False
+  return True
 
-    # Leading character must be '#'
-    if hair_color[0] != '#':
-      return False
-
-    # After leading #, chars must be numeric or a-f
-    for c in hair_color[1:7]:
-      if not (("0" <= c <= "9") or ("a" <= c <= "f")):
-        return False
-    
-    return True
-
-  def is_height_valid(height):
-
-    # Make sure last 2 chars are alphanumeric
-    if not height[-2:].isalpha():
-      return False
-    else:
-      measurement = int(height[:-2])
-    
-    units = height[-2:]
-
-    if units == "cm":
-      return 150 <= measurement <= 193
-    if units == "in":
-      return 59 <= measurement <= 76
-    else:
-      return False
-     
-  def parse_passport(passport_list):
-    "Converts each passport list of strings to a dict"
-
-    passport = {}
-    for field in passport_list:
-      passport.__setitem__(field[0:3], field[4:])
-
-    return passport
+def is_hair_color_valid(hair_color):
+  # Leading character must be '#'
+  if hair_color[0] != '#':
+    return False
   
-  def auto_validate(passport):
-    "Tests each passport-dict against the stricter auto-validation criteria"
+  if len(hair_color) != 7:
+    return False
 
-    return Passport.is_hair_color_valid(passport["hcl"]) and Passport.are_all_years_valid(passport) and Passport.is_eye_color_valid(passport["ecl"]) and Passport.is_pid_valid(passport["pid"]) and  Passport.is_height_valid(passport["hgt"])
+  # After leading #, chars must be numeric or a-f
+  for c in hair_color[1:7]:
+    if not (c.isnumeric() or ("a" <= c <= "f")):
+      return False
+  
+  return True
 
-def pre_validate(passport_list):
-  '''Filters passport list down to minimally valid passports
-  per Part 1 criteria.
+def is_height_valid(height):
 
-  Converts each prevalidated passport to a dict.
+  # Make sure last 2 chars are alphanumeric
+  if not height[-2:].isalpha():
+    return False
+  else:
+    measurement = int(height[:-2])
+  
+  units = height[-2:]
+
+  if units == "cm":
+    return 150 <= measurement <= 193
+  if units == "in":
+    return 59 <= measurement <= 76
+  else:
+    return False
+     
+def parse_passport(passport_list):
+  "Converts each passport list of strings to a dict"
+
+  passport = {}
+  for field in passport_list:
+    passport.__setitem__(field[0:3], field[4:])
+
+  return passport
+  
+def auto_validate(passport):
+  "Tests each passport-dict against the stricter auto-validation criteria, and returns True if everything passes"
+
+  checks = []
+
+  checks.append(is_hair_color_valid(passport["hcl"]))
+  checks.append(are_all_years_valid(passport))
+  checks.append(is_eye_color_valid(passport["ecl"]))
+  checks.append(is_pid_valid(passport["pid"]))
+  checks.append(is_height_valid(passport["hgt"]))
+
+  return functools.reduce(lambda x, y: x and y, checks)
+
+def prevalidate(passport):
+  '''Prevalidates a passport for Part 1 criteria.
   '''
-  valid_passports = []
+  # contains all fields
+  if len(passport) == 8:
+    return True
+  # missing one field
+  if len(passport) == 7:
+    s = "".join(passport)
+    # If missing the "cid" field, it's valid.
+    return s.find("cid") == -1
 
-  for p in passport_list:
-    # contains 6 or fewer fields? not valid
-    if len(p) <= 6:
-      continue
-    # contains all fields
-    elif len(p) == 8:
-      valid_passports.append(Passport.parse_passport(p))
-    # missing one field
-    elif len(p) == 7:
-      s = "".join(p)
-      # If missing the "cid" field, valid.
-      if s.find("cid") == -1:
-        valid_passports.append(Passport.parse_passport(p))
-    else:
-      continue
+  return False
 
-  return valid_passports
-
-
-def get_passport_list(data):
-  '''Converts passport list into a list of lists,
-  one list per passport.
+def read_prevalidate_count(data):
+  '''Divides batch into passports, and prevalidates each passport after it is batched. 
+  
+  Returns the number of prevalidated passports.
   '''
   
   # to capture the last passport in the batch
   data.append('')
 
-  all_passports = []
+  num_valid = 0
   data_len = len(data)
   passport = ""
 
@@ -122,25 +108,48 @@ def get_passport_list(data):
     passport += data[r]
     
     if data[r] == '':
-      all_passports.append(passport.split())
+      p = passport.split()
+      if prevalidate(p):
+        num_valid += 1
       passport = ""
     else:
       passport += " "
   
-  return all_passports
+  return num_valid
+
+def read_validate_count(data):
+  '''Divides batch into passports. Prevalidates each one after it is batched, and then autovalidates the prevalidated passports. 
+  
+  data: list of strings
+  Returns the number of autovalidated passports.
+  '''
+  
+  # to capture the last passport in the batch
+  data.append('')
+
+  num_valid = 0
+  data_len = len(data)
+  passport = ""
+
+  for r in range(data_len):
+    passport += data[r]
+    
+    if data[r] == '':
+      p = passport.split()
+      if prevalidate(p):
+        p = parse_passport(p)
+        if auto_validate(p):
+          num_valid += 1
+      passport = ""
+    else:
+      passport += " "
+  
+  return num_valid
 
 def part1(data):
   lines = data.splitlines()
-  passports = get_passport_list(lines)
-  prevalidated = pre_validate(passports)
-  return len(prevalidated)
-
+  return read_prevalidate_count(lines)
 
 def part2(data):
   lines = data.splitlines()
-  passport_list = get_passport_list(lines)
-  prevalidated = pre_validate(passport_list)
-
-  auto_validated = [p for p in prevalidated if Passport.auto_validate(p)]
-
-  return len(auto_validated)
+  return read_validate_count(lines)
